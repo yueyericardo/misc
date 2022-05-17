4e57b860f678a7c34250a6376fe847c0167b54a6
richard <yueyericardo@gmail.com>
2022-05-17 05:16:33 -0400

    quick save

--- 2022/functorch/functorch_jocabian.py
+++ 2022/functorch/functorch_jocabian.py
@@ -2,13 +2,16 @@ import time
 from functorch import vmap, jacrev, jacfwd, vjp
 import torch
 import torch.nn as nn
-
+import torchsnooper
+import snoop
+torchsnooper.register_snoop(verbose=True)
 _ = torch.manual_seed(0)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 D1 = 2  # x, y
 D2 = 3  # u, v, p
-B = 3000
+B = 10000
 x = torch.randn(B, D1).to(device)
+I_N = torch.eye(D2).to(device)
 
 model = nn.Sequential(
     nn.Linear(D1, 512), nn.Tanh(),
@@ -21,29 +24,62 @@ model = nn.Sequential(
 ).to(device)
 
 
+# def predict(x):
+#     torch.cuda.nvtx.range_push("forward")
+#     out = model(x)
+#     torch.cuda.nvtx.range_pop()
+#     return out, out  # return two outputs is needed for jacrev auxiliary object
+
+
 def predict(x):
     torch.cuda.nvtx.range_push("forward")
     out = model(x)
     torch.cuda.nvtx.range_pop()
-    return out, out  # return two outputs is needed for jacrev auxiliary object
-
+    return out
 
 def reference_jacobian():
     x_ = x.clone().requires_grad_()
     ones = torch.ones(B, device=x.device)
-    pred, _ = predict(x_)
+    pred = predict(x_)
     jacobian_rows = [None] * D2
     for i in range(D2):
+        # if i == 1:
+            # continue
         torch.cuda.nvtx.range_push("autograd")
         jacobian_rows[i] = torch.autograd.grad(pred[:, i], x_, ones, create_graph=True)[0]
         torch.cuda.nvtx.range_pop()
+    jacobian_rows = [j for j in jacobian_rows if j is not None]
     jacobian = torch.stack(jacobian_rows)  # [D2, B, D1]
     return jacobian, pred
 
 
+# def functorch_jacobian():
+#     x_ = x.requires_grad_()
+#     jacobian, pred = vmap(jacrev(predict, argnums=0, has_aux=True), in_dims=0)(x_)  # [B, D2, D1]
+#     return jacobian, pred
+
 def functorch_jacobian():
-    x_ = x.requires_grad_()
-    jacobian, pred = vmap(jacrev(predict, argnums=0, has_aux=True), in_dims=0)(x_)  # [B, D2, D1]
+    def get_vjp(x, v):
+        (pred, vjpfunc) = vjp(predict, x)
+        vjp()
+        return vjpfunc(v)[0], pred
+
+    x_ = x.clone().requires_grad_()
+    jacobian, pred = vmap(vmap(get_vjp, in_dims=(None, 0)), in_dims=(0, None))(x_, I_N)
+    # TODO forward has redundent works
+    pred = pred[:, 0]
+    return jacobian, pred
+
+
+def functorch_hessian():
+    def get_vjp(x, v):
+        (pred, vjpfunc) = vjp(predict, x)
+        return vjpfunc(v)[0], pred
+
+    x_ = x.clone().requires_grad_()
+    jacobian, pred = vmap(vmap(get_vjp, in_dims=(None, 0)), in_dims=(0, None))(x_, I_N)
+    # TODO
+    pred = pred[:, 0]
     return jacobian, pred
 
 
